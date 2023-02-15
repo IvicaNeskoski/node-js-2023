b8b4c1cf20a6a487e7e0534bf0fc785ebc2129cc
IvicaNeskoski <ivica.neskoski@gmail.com>
2023-02-15 22:02:09 +0100

    cas2

--- c2/index.js
+++ c2/index.js
@@ -44,3 +44,83 @@ const testFn1 = () => {
   console.log("test fn 1 initiated. This is an arrow funciotn");
 };
 testFn1();
+
+// const testFn2 = (b, a = 25) => {
+//   return a + b;
+// };
+
+// console.log(testFn2(50, 20));
+
+const testFn3 = (o = {}) => {
+  o.ime = "Zhivko";
+  console.log(o);
+};
+
+const s = { ime: "zlate", prezime: "Zlatevski" };
+testFn3(s);
+console.log(s);
+
+let ucenik = { ime: "Marko", ocena: 4 };
+let ucenik2 = ucenik;
+
+console.log(ucenik);
+ucenik.ime = "Vlado";
+console.log(ucenik);
+console.log(ucenik2);
+
+const convert = (type, value) => {
+  let returnValue = null;
+  switch (type) {
+    case "c2f":
+      return (value * 9) / 5 + 32;
+      break;
+    case "f2c":
+      return ((value - 32) * 5) / 9;
+      break;
+    default:
+      console.log("Cannont convert. The conversion type does not exist.");
+      break;
+  }
+  return returnValue;
+};
+
+let temp = 25;
+console.log(convert("c2f", temp));
+let res = convert("p2p", temp);
+console.log(res);
+
+// c2f vrednost * 9/5 +32
+// f2c (vrednost - 32) * 5/9
+
+// definirajte dva objekti na uchenici
+// da imaat ime prezime ocenka
+// dokolku prviot ucenik ima pogolema ocena od vtoriot pechatete "Ime na prv ucenik, pa prezime na prv ucenik e podobar od ime na vtor ucenik pa prezime na vtor ucenik"
+// dokolku e obratno ispecate obratno, dokolku se isti ispechatete ucenik 1 ima isto znaenje kako i ucenik 2
+
+const ucenik1 = {
+  ime: "Ivica",
+  prezime: "Neskoski",
+  ocenka: 4,
+};
+
+const ucenik3 = {
+  ime: "Pero",
+  prezime: "Perovski",
+  ocenka: 5,
+};
+
+if (ucenik1.ocenka > ucenik3.ocenka) {
+  console.log(
+    `${ucenik1.ime} ${ucenik1.prezime} e podobar od ${ucenik3.ime} ${ucenik3.prezime}`
+  );
+} else if (ucenik1.ocenka < ucenik3.ocenka) {
+  console.log(
+    `${ucenik3.ime} ${ucenik3.prezime} e podobar od ${ucenik1.ime} ${ucenik1.prezime}`
+  );
+}
+
+ucenik1.ocenka > ucenik3.ocenka
+  ? console.log("asd")
+  : ucenik1.ocenka < ucenik3.ocenka
+  ? console.log("asd")
+  : console.log(asd);
