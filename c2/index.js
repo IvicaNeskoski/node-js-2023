e047e1fbb3ac10ebc046803b73176666767abbd9
IvicaNeskoski <ivica.neskoski@gmail.com>
2023-02-15 21:10:58 +0100

    publish branch

---
+++ c2/index.js
@@ -0,0 +1,46 @@
+// console.log("Hello World");
+// console.log(5 != 5);
+// console.log(typeof 25);
+
+// const chovek = {
+//   name: "Jhon",
+//   surname: "Doe",
+// };
+
+// console.log(chovek);
+
+// a = 10;
+// console.log(a);
+a = 12;
+// console.log(a);
+const b = 15;
+// console.log(b);
+
+// let niza = [1, true, 2.5, "zdravo"];
+// console.log(niza);
+
+let student = {
+  ime: "Pero",
+  prezime: "Nakov",
+  vozrast: 25,
+};
+
+console.log(student);
+
+student.prezime = "Perovski";
+console.log(student);
+
+function testFn(a, b) {
+  console.log(" a: ", a);
+  console.log(" b: ", b);
+  return "Test fn initiated";
+}
+
+console.log(testFn(a, b));
+let test_fn = testFn(a, b);
+console.log(test_fn);
+
+const testFn1 = () => {
+  console.log("test fn 1 initiated. This is an arrow funciotn");
+};
+testFn1();
